d32e249d6ec3ece3d13674187678ab0a6c897252
Ganesh N. Sivalingam <ganesh@parkopedia.com>
2017-05-24 21:16:57 +0100

    Tidied up code and added some comments

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Convolution2D, Cropping2D, Dropout, Activation
-# from keras_tqdm import TQDMNotebookCallback
+from keras.layers import Flatten, Dense, Lambda, Convolution2D, 
+from keras.layers import Cropping2D, Dropout, Activation
 import keras
 import pickle
 from os.path import join
@@ -9,9 +9,6 @@ from scipy.misc import imresize
 import pandas as pd
 import skimage.io
 
-# DATA_PICKLE_FN = "/home/gns/Documents/data/170519_behavioural_cloning.p"
-# with open(DATA_PICKLE_FN, "rb") as ifile:
-#     images, steering_angles = pickle.load(ifile)
 
 MODEL_NAME = "./170519_05.h5"
 DATA_DIR = "/opt/data/behavioural_training"
@@ -19,7 +16,7 @@ EPOCHS = 20
 
 
 def balance_dataset(info_df, bins=250):
-    """info_df is the driving_log.csv usually"""
+    """Reduce the proportion of training images where steering  angle == 0"""
     bin_counts = sorted(np.histogram(info_df['steering'], bins=bins)[0], reverse=True)
     scale_to = bin_counts[1]
     balanced_df = pd.concat((
@@ -29,7 +26,12 @@ def balance_dataset(info_df, bins=250):
     return balanced_df
 
 
-def read_in_combine_images(info_df, offsets=[0, 0.2, -0.2], cameras=['center', 'left', 'right'], data_dir=""):
+def read_in_combine_images(info_df, offsets=[0, 0.2, -0.2],
+                           cameras=['center', 'left', 'right'], data_dir=""):
+    """
+    Reads in all images, corrects the angle for the side cameras
+    and returns the combined dataset.
+    """
     imagesd = {}
     for key in cameras:
         imagesd[key] = info_df[key].map(lambda x: skimage.io.imread(join(data_dir, x.strip())))
@@ -48,12 +50,13 @@ def read_in_combine_images(info_df, offsets=[0, 0.2, -0.2], cameras=['center', '
 
 
 def scale_crop_images(all_images):
+    """This scales images to be the same resolution as the Nvidia input"""
     all_images = [imresize(img, 1/1.6)[19: -15] for img in all_images]
     return np.array(all_images)
 
 
 def duplicate_set_with_flips(all_images, all_steering_angles):
-    print("Test")
+    """Create a duplicate dataset with flipped images and angles, append and return"""
     all_images = np.vstack((all_images, [np.flip(img, axis=1) for img in all_images]))
     all_steering_angles = np.hstack((all_steering_angles, -all_steering_angles))
     return all_images, all_steering_angles
@@ -68,12 +71,13 @@ data_dirs = [
     join(DATA_DIR, 'recoveries')
 ]
 
+# ================
+# Read in the data
 all_images, all_angles = None, None
 for dd in data_dirs[:1]:
     df = pd.read_csv(join(dd, "driving_log.csv"))
     df = balance_dataset(df)
     angles, images = read_in_combine_images(df, data_dir=dd, offsets=[0, 0.2, -0.2])
-    # images = scale_crop_images(images)
     images, angles = duplicate_set_with_flips(images, angles)
 
     if all_images is None:
@@ -83,20 +87,16 @@ for dd in data_dirs[:1]:
         all_images = np.vstack((all_images, images))
         all_angles = np.hstack((all_angles, angles))
 
-print(images.shape, angles.shape)
 
 # ================    
 # Model architecture
 model = Sequential()
-# model.add(Lambda(lambda x: x/255 - 0.5, input_shape=[66, 200, 3]))
 model.add(Lambda(lambda x: x/255 - 0.5, input_shape=[160, 320, 3]))
 model.add(Cropping2D(cropping=((50, 25), (0, 0))))
-#model.add(Lambda(lambda image: tf.image.resize_images(image, (66, 200))))
 model.add(Convolution2D(24, [5, 5], strides=(2, 2)))
 model.add(Activation('relu'))
 model.add(Convolution2D(36, [5, 5], strides=(2, 2)))
 model.add(Convolution2D(48, [3, 3], strides=(1, 1)))
-# model.add(Convolution2D(48, [5, 5], strides=(2, 2)))
 model.add(Activation('relu'))
 model.add(Convolution2D(64, [3, 3], strides=(1, 1)))
 model.add(Dropout(.1))
@@ -109,6 +109,6 @@ model.add(Dense(1))
 # ================
 # Running
 model.compile(loss='mse', optimizer='adam')
-# model.fit(np.array(images), steering_angles, validation_split=0.2, shuffle=True, epochs=7, verbose=1) 
-model.fit(all_images, all_angles, validation_split=0.2, shuffle=True, epochs=EPOCHS, verbose=1, batch_size=64)
+model.fit(all_images, all_angles, validation_split=0.2, shuffle=True,
+          epochs=EPOCHS, verbose=1, batch_size=64)
 model.save(MODEL_NAME)
